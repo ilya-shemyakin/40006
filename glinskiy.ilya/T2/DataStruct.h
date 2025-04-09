53b566adf92526ec5831f1e21bfe7065f825ce44
Глинский Илья <glinckyweb@gmail.com>
2025-04-09 16:38:41 +0300

    added output for DataStruct & tested sorting

--- glinskiy.ilya/T2/DataStruct.h
+++ glinskiy.ilya/T2/DataStruct.h
@@ -5,13 +5,17 @@
 #include <iostream>
 #include <string>
 
-
 struct DataStruct {
   unsigned long long key1;
   std::complex<double> key2;
   std::string key3;
 
-  // friend std::ostream &operator<<(std::ostream &ostream, DataStruct struct) {}
+  friend std::ostream &operator<<(std::ostream &ostream, DataStruct struc) {
+    ostream << "(:key1 " << struc.key1 << ":key2 " << abs(struc.key2)
+            << ":key3 " << struc.key3 << ":)";
+
+    return ostream;
+  }
 };
 
 #endif
--- glinskiy.ilya/T2/main.cpp
+++ glinskiy.ilya/T2/main.cpp
@@ -7,32 +7,41 @@
 
 bool comp(DataStruct a, DataStruct b) {
   if (a.key1 != b.key1)
-    return a.key1 > b.key1;
+    return a.key1 < b.key1;
 
   if (a.key2 != b.key2)
-    return abs(a.key2) > abs(b.key2);
+    return abs(a.key2) < abs(b.key2);
 
-  return a.key3.length() > b.key3.length();
+  return a.key3.length() < b.key3.length();
 }
 
 int main() {
-  std::vector<DataStruct> vector;
   DataStruct dstr1, dstr2, dstr3;
 
   dstr1.key1 = 1;
   dstr2.key1 = 20;
   dstr3.key1 = 300;
 
-  dstr1.key2 = std::complex(1, 0);
-  dstr2.key2 = std::complex(20, 0);
-  dstr3.key2 = std::complex(300, 0);
+  dstr1.key2 = std::complex<double>(1, 0);
+  dstr2.key2 = std::complex<double>(20, 0);
+  dstr3.key2 = std::complex<double>(300, 0);
 
   dstr1.key3 = "lor";
   dstr2.key3 = "lorem";
   dstr3.key3 = "lorem ipsum";
 
-  vector = {dstr2, dstr3, dstr1};
+  std::vector<DataStruct> vector = {dstr3, dstr2, dstr1};
+
+  for (int i = 0; i < vector.size(); i++) {
+    std::cout << vector[i] << "\n";
+  }
+  std::cout << "\n";
+
   std::sort(vector.begin(), vector.end(), comp);
+
+  for (int i = 0; i < vector.size(); i++) {
+    std::cout << vector[i] << "\n";
+  }
 
   return 0;
 }
