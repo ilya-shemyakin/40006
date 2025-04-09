61caf7bbacfd470ac4498d5bad7a167a0a2c63a1
Глинский Илья <glinckyweb@gmail.com>
2025-04-09 17:35:23 +0300

    upd

--- glinskiy.ilya/T2/DataStruct.h
+++ glinskiy.ilya/T2/DataStruct.h
@@ -10,12 +10,7 @@ struct DataStruct {
   std::complex<double> key2;
   std::string key3;
 
-  friend std::ostream &operator<<(std::ostream &ostream, DataStruct struc) {
-    ostream << "(:key1 " << struc.key1 << ":key2 " << abs(struc.key2)
-            << ":key3 " << struc.key3 << ":)";
-
-    return ostream;
-  }
+  friend std::ostream &operator<<(std::ostream &ostream, DataStruct struc);
 };
 
 #endif
