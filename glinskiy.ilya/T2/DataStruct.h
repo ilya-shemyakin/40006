c9294f8aa257d659c2c4252451b90dcb2aef7eb6
Глинский Илья <glinckyweb@gmail.com>
2025-04-09 16:12:52 +0300

    add comparator function

--- glinskiy.ilya/T2/DataStruct.h
+++ glinskiy.ilya/T2/DataStruct.h
@@ -2,12 +2,16 @@
 #define _DATA_STRUCT_H
 
 #include <complex>
+#include <iostream>
 #include <string>
+
 
 struct DataStruct {
   unsigned long long key1;
   std::complex<double> key2;
   std::string key3;
+
+  // friend std::ostream &operator<<(std::ostream &ostream, DataStruct struct) {}
 };
 
 #endif
--- glinskiy.ilya/T2/main.cpp
+++ glinskiy.ilya/T2/main.cpp
@@ -1,7 +1,38 @@
 #include "DataStruct.h"
-#include <iostream>
+#include <algorithm>
 #include <complex>
+#include <iostream>
+#include <string>
+#include <vector>
+
+bool comp(DataStruct a, DataStruct b) {
+  if (a.key1 != b.key1)
+    return a.key1 > b.key1;
+
+  if (a.key2 != b.key2)
+    return abs(a.key2) > abs(b.key2);
+
+  return a.key3.length() > b.key3.length();
+}
 
 int main() {
+  std::vector<DataStruct> vector;
+  DataStruct dstr1, dstr2, dstr3;
+
+  dstr1.key1 = 1;
+  dstr2.key1 = 20;
+  dstr3.key1 = 300;
+
+  dstr1.key2 = std::complex(1, 0);
+  dstr2.key2 = std::complex(20, 0);
+  dstr3.key2 = std::complex(300, 0);
+
+  dstr1.key3 = "lor";
+  dstr2.key3 = "lorem";
+  dstr3.key3 = "lorem ipsum";
+
+  vector = {dstr2, dstr3, dstr1};
+  std::sort(vector.begin(), vector.end(), comp);
+
   return 0;
 }
