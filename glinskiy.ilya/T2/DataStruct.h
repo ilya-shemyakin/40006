4f2cb25c45674ce5c7959d48a961992b8b00734f
Глинский Илья <glinckyweb@gmail.com>
2025-04-22 14:58:47 +0300

    upd DataStruct header file

--- glinskiy.ilya/T2/DataStruct.h
+++ glinskiy.ilya/T2/DataStruct.h
@@ -3,14 +3,52 @@
 
 #include <complex>
 #include <iostream>
-#include <string>
 
 struct DataStruct {
   unsigned long long key1;
   std::complex<double> key2;
   std::string key3;
-
-  friend std::ostream &operator<<(std::ostream &ostream, DataStruct struc);
 };
+
+struct DelimiterIO {
+  char exp;
+};
+
+struct LabelIO {
+  std::string exp;
+};
+
+struct UnsignedLongLongIO {
+  unsigned long long &ref;
+};
+
+struct ComplexIO {
+  std::complex<double> &ref;
+};
+
+struct StringIO {
+  std::string &ref;
+};
+
+class iofmtguard {
+public:
+  iofmtguard(std::basic_ios<char> &s);
+  ~iofmtguard();
+
+private:
+  std::basic_ios<char> &s_;
+  std::streamsize width_;
+  char fill_;
+  std::streamsize precision_;
+  std::basic_ios<char>::fmtflags fmt_;
+};
+
+std::istream &operator>>(std::istream &istream, DelimiterIO &&dest);
+std::istream &operator>>(std::istream &istream, UnsignedLongLongIO &&dest);
+std::istream &operator>>(std::istream &istream, ComplexIO &&dest);
+std::istream &operator>>(std::istream &istream, StringIO &&dest);
+std::istream &operator>>(std::istream &istream, LabelIO &&dest);
+std::istream &operator>>(std::istream &istream, DataStruct &dest);
+std::ostream &operator<<(std::ostream &ostream, const DataStruct &dest);
 
 #endif
