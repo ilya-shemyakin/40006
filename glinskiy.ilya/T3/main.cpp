61fe5f8b7561bfe4928d719e3f7f1b074d73b162
Глинский Илья <glinckyweb@gmail.com>
2025-05-07 15:21:00 +0300

    created Point and Polygon structs

--- glinskiy.ilya/T3/main.cpp
+++ glinskiy.ilya/T3/main.cpp
@@ -1,5 +1,31 @@
-#include <iostream>
+#include <algorithm>
+#include <vector>
 
-int main() {
-  std::cout << "Hello aaaa\n";
-}
+struct Point {
+  int x, y;
+
+  bool operator==(Point &other) { return x == other.x && y == other.y; }
+};
+
+struct Polygon {
+  std::vector<Point> points;
+
+  double area() {
+    if (points.size() < 3)
+      return 0.0;
+
+    double a = 0.0;
+
+    for (size_t i = 0; i < points.size(); ++i) {
+      const Point &p1 = points[i];
+      const Point &p2 = points[(i + 1) % points.size()];
+      a += (p1.x * p2.y - p2.x * p1.y);
+    }
+
+    return std::abs(a) / 2.0;
+  }
+
+  bool operator==(Polygon &other) { return points == other.points; }
+};
+
+int main() {}
