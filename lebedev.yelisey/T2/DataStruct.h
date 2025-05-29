c9e2d625ec7fbc43e166d8d5aa8b47ba004d346f
YeliseyLebedev <elisej.lebedev.06@bk.ru>
2025-05-29 18:40:05 +0300

    Исправления

--- lebedev.yelisey/T2/DataStruct.h
+++ lebedev.yelisey/T2/DataStruct.h
@@ -17,4 +17,4 @@ std::istream& operator>>(std::istream& in, DataStruct& data);
 std::ostream& operator<<(std::ostream& out, const DataStruct& data);
 void processData();
 
-#endif // DATASTRUCT_H
+#endif 
--- lebedev.yelisey/T2/main.cpp
+++ lebedev.yelisey/T2/main.cpp
@@ -2,9 +2,6 @@
 
 int main() {
 
-    // (:key1 10:key2 #c(1.0 -1.0):key3 "test":)
-    // (:key1 5:key2 #c(2.0 3.0):key3 "hello":)
-
     processData();
     return 0;
 }
