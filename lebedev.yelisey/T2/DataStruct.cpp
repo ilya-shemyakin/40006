ea46d838915fcfa2a1c92814540d5d2a67d1e211
YeliseyLebedev <elisej.lebedev.06@bk.ru>
2025-09-17 12:30:43 +0300

    Update DataStruct.cpp

--- lebedev.yelisey/T2/DataStruct.cpp
+++ lebedev.yelisey/T2/DataStruct.cpp
@@ -13,10 +13,8 @@ std::istream& operator>>(std::istream& in, DataStruct& data) {
     size_t key1_start = line.find(":key1 ");
     size_t key2_start = line.find(":key2 #c(");
     size_t key3_start = line.find(":key3 \"");
-    
-    if (key1_start == std::string::npos || 
-        key2_start == std::string::npos || 
-        key3_start == std::string::npos) {
+
+    if (key1_start == std::string::npos || key2_start == std::string::npos || key3_start == std::string::npos) {
         in.setstate(std::ios::failbit);
         return in;
     }
@@ -24,10 +22,8 @@ std::istream& operator>>(std::istream& in, DataStruct& data) {
     try {
         size_t key1_end = line.find(':', key1_start + 1);
         std::string key1_str = line.substr(key1_start + 6, key1_end - (key1_start + 6));
-        
-        if (key1_str.size() >= 2 && 
-            (key1_str.substr(key1_str.size() - 2) == "ll" || 
-             key1_str.substr(key1_str.size() - 2) == "LL")) {
+
+        if (key1_str.size() >= 2 && (key1_str.substr(key1_str.size() - 2) == "ll" || key1_str.substr(key1_str.size() - 2) == "LL")) {
             key1_str = key1_str.substr(0, key1_str.size() - 2);
         }
         data.key1 = std::stoll(key1_str);
@@ -59,32 +55,24 @@ std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
 
 void processData() {
     std::vector<DataStruct> dataVector;
-    
-    std::copy(
-        std::istream_iterator<DataStruct>(std::cin),
-        std::istream_iterator<DataStruct>(),
-        std::back_inserter(dataVector)
+
+    std::copy(std::istream_iterator<DataStruct>(std::cin), std::istream_iterator<DataStruct>(), std::back_inserter(dataVector)
     );
 
-    std::sort(dataVector.begin(), dataVector.end(), 
-        [](const DataStruct& a, const DataStruct& b) {
+    std::sort(dataVector.begin(), dataVector.end(), [](const DataStruct& a, const DataStruct& b) {
             if (a.key1 != b.key1) {
                 return a.key1 < b.key1;
             }
-            
+
             double abs_a = std::abs(a.key2);
             double abs_b = std::abs(b.key2);
             if (std::abs(abs_a - abs_b) > 1e-10) {
                 return abs_a < abs_b;
             }
-            
+
             return a.key3.length() < b.key3.length();
         }
     );
 
-    std::copy(
-        dataVector.begin(),
-        dataVector.end(),
-        std::ostream_iterator<DataStruct>(std::cout, "\n")
-    );
+    std::copy(dataVector.begin(), dataVector.end(), std::ostream_iterator<DataStruct>(std::cout, "\n"));
 }
