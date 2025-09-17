6ff48c86ec498c0793d3837edf9bab19c22bf980
YeliseyLebedev <elisej.lebedev.06@bk.ru>
2025-09-17 13:21:25 +0300

    Update DataStruct.cpp

--- lebedev.yelisey/T2/DataStruct.cpp
+++ lebedev.yelisey/T2/DataStruct.cpp
@@ -6,49 +6,55 @@
 
 std::istream& operator>>(std::istream& in, DataStruct& data) {
     std::string line;
-    while (std::getline(in, line)) {
+    if (!std::getline(in, line)) {
+        return in;
+    }
 
-        if (line.find(":key1 ") == std::string::npos) continue;
-        if (line.find(":key2 #c(") == std::string::npos) continue;
-        if (line.find(":key3 \"") == std::string::npos) continue;
+    size_t key1_start = line.find(":key1 ");
+    size_t key2_start = line.find(":key2 #c(");
+    size_t key3_start = line.find(":key3 \"");
 
-        size_t key1_start = line.find(":key1 ");
+    if (key1_start == std::string::npos ||
+        key2_start == std::string::npos ||
+        key3_start == std::string::npos) {
+        in.setstate(std::ios::failbit);
+        return in;
+    }
+
+    try {
         size_t key1_end = line.find(':', key1_start + 1);
         std::string key1_str = line.substr(key1_start + 6, key1_end - (key1_start + 6));
 
-        if (key1_str.size() < 2 || (key1_str.substr(key1_str.size() - 2) != "ll" && key1_str.substr(key1_str.size() - 2) != "LL")) {
-            continue;
-        }
-
-        try {
+        if (key1_str.size() >= 2 &&
+            (key1_str.substr(key1_str.size() - 2) == "ll" ||
+                key1_str.substr(key1_str.size() - 2) == "LL")) {
             key1_str = key1_str.substr(0, key1_str.size() - 2);
-            data.key1 = std::stoll(key1_str);
+        }
+        data.key1 = std::stoll(key1_str);
 
-            size_t key2_start = line.find(":key2 #c(");
-            size_t key2_end = line.find(')', key2_start);
-            std::string key2_str = line.substr(key2_start + 9, key2_end - (key2_start + 9));
-            std::istringstream iss(key2_str);
-            std::string real_str, imag_str;
-            iss >> real_str >> imag_str;
-            double real = std::stod(real_str);
-            double imag = std::stod(imag_str);
-            data.key2 = std::complex<double>(real, imag);
+        size_t key2_end = line.find(')', key2_start);
+        std::string key2_str = line.substr(key2_start + 9, key2_end - (key2_start + 9));
+        std::istringstream iss(key2_str);
+        std::string real_str, imag_str;
+        iss >> real_str >> imag_str;
+        double real = std::stod(real_str);
+        double imag = std::stod(imag_str);
+        data.key2 = std::complex<double>(real, imag);
 
-            size_t key3_start = line.find(":key3 \"");
-            size_t key3_end = key3_start + 7;
-            int quote_count = 0;
-            while (key3_end < line.size() && quote_count < 2) {
-                if (line[key3_end] == '"') quote_count++;
-                key3_end++;
+        size_t key3_end = key3_start + 7;
+        int quote_count = 0;
+        while (key3_end < line.size() && quote_count < 2) {
+            if (line[key3_end] == '"') {
+                quote_count++;
             }
-            data.key3 = line.substr(key3_start + 7, key3_end - (key3_start + 7) - 1);
-
-            break;
+            key3_end++;
         }
-        catch (...) {
-            continue;
-        }
+        data.key3 = line.substr(key3_start + 7, key3_end - (key3_start + 7) - 1);
     }
+    catch (...) {
+        in.setstate(std::ios::failbit);
+    }
+
     return in;
 }
 
@@ -67,17 +73,17 @@ void processData() {
     );
 
     std::sort(dataVector.begin(), dataVector.end(), [](const DataStruct& a, const DataStruct& b) {
-        if (a.key1 != b.key1) {
-            return a.key1 < b.key1;
-        }
+            if (a.key1 != b.key1) {
+                return a.key1 < b.key1;
+            }
 
-        double abs_a = std::abs(a.key2);
-        double abs_b = std::abs(b.key2);
-        if (std::abs(abs_a - abs_b) > 1e-10) {
-            return abs_a < abs_b;
-        }
+            double abs_a = std::abs(a.key2);
+            double abs_b = std::abs(b.key2);
+            if (std::abs(abs_a - abs_b) > 1e-10) {
+                return abs_a < abs_b;
+            }
 
-        return a.key3.length() < b.key3.length();
+            return a.key3.length() < b.key3.length();
         }
     );
 
