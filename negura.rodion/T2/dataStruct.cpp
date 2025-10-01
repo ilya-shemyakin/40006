f496b2a7e29067ea1896433c1b5ba062bc3f38c7
Negura Rodion <negurarodion300@gmail.com>
2025-10-02 02:39:26 +0300

    Fix record parsing: make suffix checks flexible and handle unsupported formats

--- negura.rodion/T2/dataStruct.cpp
+++ negura.rodion/T2/dataStruct.cpp
@@ -4,6 +4,7 @@
 #include <algorithm>
 #include <limits>
 #include <iomanip>
+#include <sstream>
 #include "DataStruct.h"
 
 namespace nspace {
@@ -16,7 +17,6 @@ namespace nspace {
   const std::string KEY1_LABEL = "key1";
   const std::string KEY2_LABEL = "key2";
   const std::string KEY3_LABEL = "key3";
-  const std::string ULL_SUFFIX = "ull";
 
   std::istream& operator>>(std::istream& input, DelimiterIO&& destination) {
     std::istream::sentry sentry(input);
@@ -40,15 +40,17 @@ namespace nspace {
 
     double value = 0.0;
     input >> value;
+
     if (!input) {
       return input;
     }
 
+    
     char suffix = '\0';
-    input >> suffix;
-    if (suffix != DOUBLE_SUFFIX && suffix != std::toupper(DOUBLE_SUFFIX)) {
-      input.setstate(std::ios::failbit);
-      return input;
+    if (input >> suffix) {
+      if (suffix != DOUBLE_SUFFIX && suffix != std::toupper(DOUBLE_SUFFIX)) {
+        input.putback(suffix); 
+      }
     }
 
     destination.reference = value;
@@ -63,17 +65,21 @@ namespace nspace {
 
     unsigned long long value = 0;
     input >> value;
+
     if (!input) {
       return input;
     }
 
-
-    char u, l1, l2;
-    input >> u >> l1 >> l2;
-
-    if (u != 'u' || l1 != 'l' || l2 != 'l') {
-      input.setstate(std::ios::failbit);
-      return input;
+    
+    char u = '\0', l1 = '\0', l2 = '\0';
+    if (input >> u >> l1 >> l2) {
+      if (!(u == 'u' && l1 == 'l' && l2 == 'l') &&
+        !(u == 'U' && l1 == 'L' && l2 == 'L')) {
+        
+        input.putback(l2);
+        input.putback(l1);
+        input.putback(u);
+      }
     }
 
     destination.reference = value;
@@ -90,6 +96,7 @@ namespace nspace {
     if (!input) {
       return input;
     }
+
     std::getline(input, destination.reference, QUOTE_CHAR);
     return input;
   }
@@ -118,42 +125,51 @@ namespace nspace {
     bool isKey2Found = false;
     bool isKey3Found = false;
 
+    
+    std::ios_base::iostate state = input.rdstate();
+
     input >> DelimiterIO{ OPEN_BRACKET } >> DelimiterIO{ COLON };
 
-    while (input) {
-      if (input.peek() == CLOSE_BRACKET) {
-        input.ignore();
-        break;
-      }
-
+    while (input && input.peek() != CLOSE_BRACKET && input.peek() != EOF) {
       std::string fieldName;
       if (!(input >> fieldName)) {
         break;
       }
 
       if (fieldName == KEY1_LABEL) {
-        input >> DoubleLiteralIO{ inputData.key1 } >> DelimiterIO{ COLON };
-        isKey1Found = true;
+        if (input >> DoubleLiteralIO{ inputData.key1 } >> DelimiterIO{ COLON }) {
+          isKey1Found = true;
+        }
       }
       else if (fieldName == KEY2_LABEL) {
-        input >> UnsignedLongLongLiteralIO{ inputData.key2 } >> DelimiterIO{ COLON };
-        isKey2Found = true;
+        if (input >> UnsignedLongLongLiteralIO{ inputData.key2 } >> DelimiterIO{ COLON }) {
+          isKey2Found = true;
+        }
       }
       else if (fieldName == KEY3_LABEL) {
-        input >> StringIO{ inputData.key3 } >> DelimiterIO{ COLON };
-        isKey3Found = true;
+        if (input >> StringIO{ inputData.key3 } >> DelimiterIO{ COLON }) {
+          isKey3Found = true;
+        }
       }
       else {
-        input.setstate(std::ios::failbit);
-        break;
+        
+        std::string temp;
+        std::getline(input, temp, ':');
+        input.putback(':');
       }
+    }
+
+    
+    if (input && input.peek() == CLOSE_BRACKET) {
+      input >> DelimiterIO{ CLOSE_BRACKET };
     }
 
     if (input && isKey1Found && isKey2Found && isKey3Found) {
       destination = inputData;
     }
     else {
-      input.setstate(std::ios::failbit);
+      
+      input.setstate(state | std::ios::failbit);
     }
 
     return input;
@@ -167,7 +183,7 @@ namespace nspace {
 
     IoFormatGuard formatGuard(output);
     output << "(:key1 " << source.key1 << "d:key2 " << source.key2
-      << "ull :key3 \"" << source.key3 << "\":)";
+      << "ull:key3 \"" << source.key3 << "\":)";
     return output;
   }
 
