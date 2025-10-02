8a292bddb5ecd09595a12da1a4cd1ccc720274e2
Negura Rodion <negurarodion300@gmail.com>
2025-10-02 22:59:53 +0300

    Add T2 implementation for variant 15 (DBL LIT + ULL LIT)

--- negura.rodion/T2/main.cpp
+++ negura.rodion/T2/main.cpp
@@ -6,30 +6,29 @@
 #include "DataStruct.h"
 
 int main() {
-  std::vector<nspace::DataStruct> data;
+    std::vector<nspace::DataStruct> data;
 
-  while (!std::cin.eof()) {
-    std::copy(
-      std::istream_iterator<nspace::DataStruct>(std::cin),
-      std::istream_iterator<nspace::DataStruct>(),
-      std::back_inserter(data)
-    );
+    while (!std::cin.eof()) {
+        std::copy(
+            std::istream_iterator<nspace::DataStruct>(std::cin),
+            std::istream_iterator<nspace::DataStruct>(),
+            std::back_inserter(data)
+        );
 
-    if (std::cin.fail() && !std::cin.eof()) {
-      std::cin.clear();
-      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if (std::cin.fail() && !std::cin.eof()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        }
     }
-  }
 
-  if (!data.empty()) {
-    std::sort(data.begin(), data.end(), nspace::compareDataStruct);
-    std::copy(data.begin(), data.end(),
-      std::ostream_iterator<nspace::DataStruct>(std::cout, "\n"));
-    std::cout << "Atleast one supported record type" << std::endl;
-  }
-  else {
-    std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped" << std::endl;
-  }
+    if (!data.empty()) {
+        std::sort(data.begin(), data.end(), nspace::compareDataStruct);
+        std::copy(data.begin(), data.end(),
+                  std::ostream_iterator<nspace::DataStruct>(std::cout, "\n"));
+        std::cout << "Atleast one supported record type" << std::endl;
+    } else {
+        std::cout << "Looks like there is no supported record. Cannot determine input. Test skipped" << std::endl;
+    }
 
-  return 0;
+    return 0;
 }
