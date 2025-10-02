ca8f63600bbc32d5f4cb966c6c1222e3a3f0208c
Negura Rodion <negurarodion300@gmail.com>
2025-10-02 23:23:16 +0300

    Fix T2 implementation for variant 15 (DBL LIT + ULL LIT)
    - Add DoubleIO parser for key1 with 'd' suffix
    - Keep UllLitIO parser for key2 with 'ull' suffix
    - Fix output formatting and comparison function
    - Use proven code structure from working variant

--- negura.rodion/T2/dataStruct.cpp
+++ negura.rodion/T2/dataStruct.cpp
@@ -1,138 +1,155 @@
-#include <iostream>
+#include <algorithm>
+#include <sstream>
 #include <string>
+#include <cassert>
+#include <iterator>
+#include <vector>
+#include <utility>
 #include <iomanip>
 #include "DataStruct.h"
+#include <cstring>
 
 namespace nspace {
-    std::istream& operator>>(std::istream& in, DelimeterIO&& dest) {
-        std::istream::sentry sentry(in);
-        if (!sentry) {
-            return in;
-        }
-
-        char c;
-        in >> c;
-        if (in && (c != dest.exp)) {
-            in.setstate(std::ios::failbit);
-        }
-        return in;
+  std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
     }
-
-    std::istream& operator>>(std::istream& in, StringIO&& dest) {
-        std::istream::sentry sentry(in);
-        if (!sentry) {
-            return in;
-        }
-        return std::getline(in >> DelimeterIO{ '"' }, dest.ref, '"');
+    char c = '0';
+    in >> c;
+    if (in && (c != dest.exp))
+    {
+      in.setstate(std::ios::failbit);
     }
+    return in;
+  }
 
-    std::istream& operator>>(std::istream& in, DoubleIO&& dest) {
-        std::istream::sentry sentry(in);
-        if (!sentry) {
-            return in;
-        }
-        return in >> dest.ref >> DelimeterIO{ 'd' };
+  std::istream& operator>>(std::istream& in, StringIO&& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
     }
+    return std::getline(in >> DelimiterIO{ '"' }, dest.ref, '"');
+  }
 
-    std::istream& operator>>(std::istream& in, UllLitIO&& dest) {
-        std::istream::sentry sentry(in);
-        if (!sentry) return in;
-
-        unsigned long long value;
-        in >> value;
-
-        if (!in) {
-            return in;
-        }
-
-        char u, l1, l2;
-        in >> u >> l1 >> l2;
-
-        if (!in || !((u == 'u' && l1 == 'l' && l2 == 'l') ||
-                     (u == 'U' && l1 == 'L' && l2 == 'L'))) {
-            in.setstate(std::ios::failbit);
-            return in;
-        }
-
-        dest.ref = value;
-        return in;
+  std::istream& operator>>(std::istream& in, DoubleIO&& dbl) {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
     }
-
-    std::istream& operator>>(std::istream& in, DataStruct& dest) {
-        std::istream::sentry sentry(in);
-        if (!sentry) {
-            return in;
-        }
-
-        DataStruct temp;
-        in >> DelimeterIO{ '(' } >> DelimeterIO{ ':' };
-
-        std::string key;
-        bool hasKey1 = false, hasKey2 = false, hasKey3 = false;
-
-        while (true) {
-            in >> key;
-            if (!in) break;
-
-            if (key == "key1") {
-                in >> DoubleIO{ temp.key1 } >> DelimeterIO{ ':' };
-                hasKey1 = true;
-            }
-            else if (key == "key2") {
-                in >> UllLitIO{ temp.key2 } >> DelimeterIO{ ':' };
-                hasKey2 = true;
-            }
-            else if (key == "key3") {
-                in >> StringIO{ temp.key3 } >> DelimeterIO{ ':' };
-                hasKey3 = true;
-            }
-            else if (key == ")") {
-                break;
-            }
-            else {
-                in.setstate(std::ios::failbit);
-                break;
-            }
-        }
-
-        if (in && hasKey1 && hasKey2 && hasKey3) {
-            dest = temp;
-        }
-        else {
-            in.setstate(std::ios::failbit);
-        }
-
-        return in;
+    double value = 0.0;
+    in >> value;
+    if (!in) {
+      return in;
     }
-
-    std::ostream& operator<<(std::ostream& out, const DataStruct& data) {
-        std::ostream::sentry sentry(out);
-        if (!sentry) {
-            return out;
-        }
-
-        iofmtguard guard(out);
-        out << "(:key1 " << std::fixed << std::setprecision(1) << data.key1 << "d"
-            << ":key2 " << data.key2 << "ull"
-            << ":key3 \"" << data.key3 << "\":)";
-        return out;
+    char d;
+    in >> d;
+    if (d != 'd' && d != 'D') {
+      in.setstate(std::ios::failbit);
+      return in;
     }
+    dbl.num = value;
+    return in;
+  }
 
-    iofmtguard::iofmtguard(std::basic_ios<char>& s) :
-        s_(s), width_(s.width()), fill_(s.fill()),
-        precision_(s.precision()), fmt_(s.flags())
-    {}
-
-    iofmtguard::~iofmtguard() {
-        s_.width(width_);
-        s_.fill(fill_);
-        s_.precision(precision_);
-        s_.flags(fmt_);
+  std::istream& operator>>(std::istream& in, UllLitIO&& lit) {
+    std::istream::sentry sentry(in);
+    if (!sentry)
+    {
+      return in;
     }
-
-    bool compareDataStruct(const DataStruct& a, const DataStruct& b) {
-        if (a.key1 != b.key1) return a.key1 < b.key1;
-        if (a.key2 != b.key2) return a.key2 < b.key2;
-        return a.key3.length() < b.key3.length();
+    unsigned long long value = 0;
+    in >> value;
+    if (!in) {
+      return in;
     }
+    std::string s;
+    std::getline(in, s, ':');
+    if (s != "ull" && s != "ULL") {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    in.unget();
+    lit.num = value;
+    return in;
+  }
+
+  std::istream& operator>>(std::istream& in, DataStruct& dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) {
+      return in;
+    }
+    DataStruct input;
+
+    in >> DelimiterIO{ '(' } >> DelimiterIO{ ':' };
+    bool f1 = false;
+    bool f2 = false;
+    bool f3 = false;
+    std::string inp;
+    while (in.peek() != ')' && in) {
+      in >> inp;
+      if (inp == "key1") {
+        in >> DoubleIO{ input.key1 };
+        in >> DelimiterIO{ ':' };
+        f1 = true;
+      }
+      else if (inp == "key2") {
+        in >> UllLitIO{ input.key2 };
+        in >> DelimiterIO{ ':' };
+        f2 = true;
+      }
+      else if (inp == "key3") {
+        in >> StringIO{ input.key3 };
+        in >> DelimiterIO{ ':' };
+        f3 = true;
+      }
+      else {
+        in.setstate(std::ios::failbit);
+      }
+    }
+    in >> DelimiterIO{ ')' };
+    if (f1 && f2 && f3 && in)
+      dest = input;
+    else
+      in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  std::ostream& operator<<(std::ostream& out, const DataStruct& dest) {
+    std::ostream::sentry sentry(out);
+    if (!sentry)
+    {
+      return out;
+    }
+    iofmtguard guard(out);
+    out << "(:key1 " << std::fixed << std::setprecision(1) << dest.key1 << "d:key2 " << dest.key2 << "ull:key3 \"" << dest.key3 << "\":)";
+    return out;
+  }
+
+  bool compare(const DataStruct& a, const DataStruct& b) {
+    if (a.key1 != b.key1) {
+      return a.key1 < b.key1;
+    }
+    if (a.key2 != b.key2) {
+      return a.key2 < b.key2;
+    }
+    return a.key3.length() < b.key3.length();
+  }
+
+  iofmtguard::iofmtguard(std::basic_ios<char>& s) :
+    s_(s),
+    width_(s.width()),
+    fill_(s.fill()),
+    precision_(s.precision()),
+    fmt_(s.flags())
+  {}
+  iofmtguard::~iofmtguard() {
+    s_.width(width_);
+    s_.fill(fill_);
+    s_.precision(precision_);
+    s_.flags(fmt_);
+  }
 }
